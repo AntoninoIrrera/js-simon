e77dcc40d2860b8929f2961949991f30256a8294
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-03 23:06:21 +0100

    aggiunta secondi minuti ore e perfezionato tempo come reale cronometro

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -18,15 +18,24 @@
 
 
 
-const tempo = document.getElementById('tempo');
+const tempoMillisecondi = document.getElementById('tempoMillisecondi');
+const tempoSecondi = document.getElementById('tempoSecondi');
+const tempoMinuti = document.getElementById('tempoMinuti');
+const tempoOre = document.getElementById('tempoOre');
 const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
 
-let contatore = 0;
+let millisecondi = 0;
 let pausa = false;
 let inizio;
+let secondi = 0;
+let minuti = 0;
+let ore = 0;
+
+
+
 
 start.addEventListener("click", function(){
 
@@ -35,12 +44,28 @@ start.addEventListener("click", function(){
         
         inizio = setInterval(function(){
             
-            contatore++;
-            tempo.innerHTML = contatore;
+            millisecondi = millisecondi + 4;
+            tempoMillisecondi.innerHTML = "Millisecondi: " + millisecondi;
     
+            if(millisecondi == 1000){
+                secondi++;
+                millisecondi = 0;
+                tempoSecondi.innerHTML = "Secondi: " + secondi;
+            }
+            if(secondi == 60){
+                minuti++;
+                secondi = 0;
+                tempoMinuti.innerHTML = "Minuti: " + minuti;
+            }
+            if(minuti == 60){
+                ore++;
+                minuti = 0;
+                tempoOre.innerHTML = "Ore: " + ore;
+            }
+
+
     
-    
-        }, 1);
+        },1);
 
         pausa = true;
 
@@ -63,8 +88,17 @@ pause.addEventListener("click", function(){
 
 reset.addEventListener("click", function(){
 
-    contatore = 0;
-    tempo.innerHTML = contatore;
+    secondi = 0;
+    millisecondi = 0;
+    minuti = 0;
+    ore = 0;
+    tempoMillisecondi.innerHTML = "Millisecondi: " + millisecondi;
+    tempoSecondi.innerHTML = "Secondi: " + secondi;
+    tempoMinuti.innerHTML = "Minuti: " + minuti;
+    tempoOre.innerHTML = "Ore: " + ore;
+
+
+
 
 
 })
